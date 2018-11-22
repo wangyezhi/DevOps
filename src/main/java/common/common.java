ff5d1392966b7942be07770f3064de9a1baa80f8
hogan.wang <hogan.wang@Zu-PC77.bamboonetworks.com>
2018-11-22 09:53:35 +0800

    Testing Hygieia and GitHub communications!!!

--- src/main/java/common/common.java
+++ src/main/java/common/common.java
@@ -2,6 +2,11 @@ package common;
 
 public class common {
 	public void show() {
-		System.out.println("Testing jenkins and slack communications!");
+		System.out.println("Testing jenkins and slack communications!!!");
 	}
+	
+	public void display() {
+		System.out.println("Testing Hygieia and GitHub communications!!!");
+	}
+	
 }
